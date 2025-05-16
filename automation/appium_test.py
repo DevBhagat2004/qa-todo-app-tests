949cc5240cdfd12a077420e4e169e19fe12bb918
DevBhagat2004 <163807837+DevBhagat2004@users.noreply.github.com>
2025-05-16 08:10:02 -0500

    Update appium_test.py Expanded the Automation

--- automation/appium_test.py
+++ automation/appium_test.py
@@ -1,16 +1,42 @@
+import pytest
 from appium import webdriver
+from appium.webdriver.common.touch_action import TouchAction
 
-caps = {
-    "platformName": "Android",
-    "appium:deviceName": "Pixel 6",
-    "appium:app": "https://github.com/amitshekhariitbhu/ToDo/blob/master/apk/todo.apk?raw=true",
-    "automationName": "UiAutomator2"
-}
-
-def test_add_task():
-    driver = webdriver.Remote("http://localhost:4723", caps)
-    driver.find_element("id", "com.todoist:id/add_task").click()
-    driver.find_element("id", "com.todoist:id/text").send_keys("Automated test")
-    driver.find_element("id", "com.todoist:id/submit").click()
-    assert "Automated test" in driver.page_source
+@pytest.fixture
+def driver():
+    caps = {
+        'platformName': 'Android',
+        'deviceName': 'Pixel 6',
+        'app': 'https://example.com/todo_app.apk',
+        'automationName': 'UiAutomator2'
+    }
+    driver = webdriver.Remote('http://localhost:4723', caps)
+    yield driver
     driver.quit()
+
+def test_add_task(driver):
+    driver.find_element_by_id('add_btn').click()
+    driver.find_element_by_id('task_input').send_keys('Automated test')
+    driver.find_element_by_id('save_btn').click()
+    assert 'Automated test' in driver.page_source
+
+def test_swipe_to_delete(driver):
+    # Add test task
+    driver.find_element_by_id('add_btn').click()
+    driver.find_element_by_id('task_input').send_keys('To delete')
+    driver.find_element_by_id('save_btn').click()
+    
+    # Swipe to delete
+    task = driver.find_element_by_xpath('//android.widget.TextView[@text="To delete"]')
+    TouchAction(driver).press(task).wait(1000).move_to(x=-300, y=0).release().perform()
+    driver.find_element_by_id('delete_btn').click()
+    assert 'To delete' not in driver.page_source
+
+def test_offline_behavior(driver):
+    # Toggle airplane mode
+    driver.set_network_connection(0)
+    try:
+        driver.find_element_by_id('add_btn').click()
+        assert driver.find_element_by_id('offline_warning').is_displayed()
+    finally:
+        driver.set_network_connection(6)
